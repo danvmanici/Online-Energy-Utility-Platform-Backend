22dd32963061c7bc1f02f2a81af048b44cb036c9
Dan <danvmanici@gmail.com>
2021-11-09 20:44:54 +0200

    w

--- src/main/java/ro/tuc/ds2020/entities/Client.java
+++ src/main/java/ro/tuc/ds2020/entities/Client.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 public class Client implements Serializable{
 
     private static final long serialVersionUID = 1L;
-
+//
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
